6527d9d6318515e911b9a35f10660ce39692e4dc
andrewcgaitskell <andrew@gaitskell.com>
2020-06-26 13:55:06 +0100

    Create createpeer.js

---
+++ createpeer.js
@@ -0,0 +1,37 @@
+global.window = {document: {createElementNS: () => {return {}} }};
+global.navigator = {};
+global.btoa = () => {};
+
+const peerJs = require('peerjs-nodejs');
+const peer2 = peerJs('andrewcgaitskell',{
+  host: '35.214.117.188' ,
+  port: 9000 ,
+  path: '/myapp'
+});
+
+
+const http = require('http');
+
+
+http.createServer(function (req, res) {
+
+  
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  
+  res.write('Creating Peer!');
+  
+  var conn = peer2.connect('andrewcgaitskell');
+// on open will be launch when you successfully connect to PeerServer
+conn.on('open', function(){
+  // here you have conn.id
+  conn.send('hi!');
+});
+  
+  
+  res.write('Created and Connected Peer!');
+  
+  res.end();
+  
+  
+
+}).listen(8080);
