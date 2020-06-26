1c367272eaccddc36093e3d8efb5eba2ca10e576
andrewcgaitskell <andrew@gaitskell.com>
2020-06-26 10:55:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8081
 
-app.get('/', (req, res) => res.send('Root Express 3000!'))
+app.get('/', (req, res) => res.send('Root Express 8081!'))
 
-app.get('/hello', (req, res) => res.send('Hello World Express 3000!'))
+app.get('/hello', (req, res) => res.send('Hello World Express 8081!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
