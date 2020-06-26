c3692c1a6ed7792873f6fa673977382a2c87d1df
andrewcgaitskell <andrew@gaitskell.com>
2020-06-26 09:59:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/nodeapp', (req, res) => res.send('Hello World Express 3000!'))
+app.get('/', (req, res) => res.send('Hello World Express 3000!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
